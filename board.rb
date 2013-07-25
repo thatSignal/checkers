90cfa95ed5b30a6f22c87b06854bb3210a55b1b7
App Academy Student <student@appacademy.io>
2013-07-25 16:08:48 -0700

    jumps amd movement working

--- board.rb
+++ board.rb
@@ -1,4 +1,4 @@
-#first commit
+
 require_relative "./piece"
 
 class Board
@@ -6,7 +6,7 @@ class Board
   attr_accessor :rows
 
   def initialize
-    @rows = Array.new(8) { Array.new(8) { " " } }
+    @rows = Array.new(8) { Array.new(8) { NullPiece.new } }
   end
 
   def []=(*pos, piece)
@@ -21,26 +21,46 @@ class Board
     @rows[i][j]
   end
 
-  def empty_at?(pos)
+
+
+  def empty_at?(pos) # << ARRAY
     i, j = pos
-    return true if @rows[i][j] == nil
+    return true if self[i, j] == nil || self[i, j].is_a?(NullPiece)
     false
   end
 
-  def move(origin, destination) #takes two arrays
+  def move!(origin, destination) #takes two arrays
     oi, oj = origin
     di, dj = destination
 
-    piece = self[oi, oj]
+    moving_piece = self[oi, oj]
+    self[di, dj] = moving_piece
+    moving_piece.position = [di, dj]
+    self[oi, oj] = NullPiece.new
+  end
 
+  def team_pieces(color)
+    all_pieces.select { |piece| piece.color == color }
+  end
+
+  def all_pieces
+    @rows.flatten.select { |piece| piece.color == :red || piece.color == :white }
+  end
+
+  def on_board?(pos) # << ARRAY
+    i, j = pos
+    return false if i < 0 || i > 7
+    return false if j < 0 || j > 7
+    true
   end
 
   def to_s
-    puts "--------------------------------"
-    @rows.each do |row|
-      puts "| #{row.join(" | ")} |\n"
-      puts "--------------------------------"
+    puts "  --------------------------------"
+    @rows.each_with_index do |row, i|
+      puts "#{i}| #{row.join(" | ")} |\n"
+      puts "  --------------------------------"
     end
+    puts "   0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 -"
   end
 
   def set_pieces
@@ -73,12 +93,31 @@ end # END BOARD
 #___________________________________________________________________________
 
 $b = Board.new
-$b.set_pieces
+#$b.set_pieces
+r1 = Piece.new($b, :red)
+r2 = Piece.new($b, :red)
 
-puts $b[0, 1].class
-piece = $b[0, 1]
-puts piece.slide_positions
+w1 = Piece.new($b, :white)
+w2 = Piece.new($b, :white)
+w3 = Piece.new($b, :white)
+
+$b[2, 1] = r1
+$b[3, 2] = w1
+$b[3, 0] = w2
+#$b[4, 3] = w3
 puts $b
+puts r1.kill_jumps.inspect
+puts r1.can_kill?
+#puts $b.all_pieces.inspect
 
-puts $b[0, 1]
+# $b[4,3] = w1
+# w1.position = [4,4]
+# $b[3,2] = NullPiece.new
+
+$b.move!([3,2], [4,3])
+
+
+
+#$b[]
+puts $b
 
--- piece.rb
+++ piece.rb
@@ -8,46 +8,67 @@ class Piece
     @color = color
   end
 
-  MOVE_DELTAS = [
-    [-1, 1],
-    [1, 1]
-  ]
 
-  JUMP_DELTAS = [
-    [-2, 2],
-    [2, 2]
-  ]
+  #RETURNS ARRAY OF POSITIONS THE PLAYER MAY MOVE TO
+  def legal_moves
+    if can_kill
+      kill_jumps
+    else
+      slide_positions.select { |pos| @board.on_board?(pos) }
+    end
+  end
 
-  def can_jump?
-    diagonals = slide_positions
+  def can_kill?
+    not kill_jumps.empty?
+  end
 
-    diagonals.each do |pos|
-      if @board[*pos] != nil || @board[*pos].color != @color
-        return true
-      else
-        return false
+  #RETURNS ARRAY OF ADJACENT ENEMIES
+  def kill_jumps
+    slides = slide_positions
+    jumps = jump_positions
+    kill_jumps = []
+
+    slides.each do |s_direction, s_pos|
+      next if @board.empty_at?(s_pos)
+      jumps.each do |j_direction, j_pos|
+        if s_direction == j_direction  &&  @board[*s_pos].color == enemy_color && @board.empty_at?(j_pos)
+          kill_jumps << j_pos #@board[*s_pos]
+        end
       end
     end
 
+    kill_jumps
   end
 
   def slide_positions
     i, j = @position
-    slides = [[i + color_dir , j - 1], [i + color_dir , j + 1]]
-    slides
+    slides = {
+      :f_left => [i + color_dir , j - 1],
+      :f_right => [i + color_dir , j + 1]
+    }
+    slides_copy = slides.dup
+    slides_copy.delete_if { |key, value| @board.on_board?(value) == false }
+
+  end
+
+  def jump_positions
+    i, j = @position
+    jumps = {
+      :f_left => [i + 2 * color_dir , j - 2],
+      :f_right => [i + 2 * color_dir , j + 2]
+    }
+    jumps_copy = jumps.dup
+    jumps_copy.delete_if { |key, value| @board.on_board?(value) == false }
   end
 
   def enemy_color
     @color == :red ? :white : :red
   end
 
-
-
-
-  private #____________
-  def sum_positions(pos1, pos2) #takes two arrays
-    [pos1[0] + pos2[0], pos1[1] + pos2[1]]
-  end
+  private #___
+  # def sum_positions(pos1, pos2) #takes two arrays
+#     [pos1[0] + pos2[0], pos1[1] + pos2[1]]
+#   end
 
   def color_dir
     (@color == :red) ? 1 : -1
@@ -56,5 +77,36 @@ class Piece
   def to_s
     (@color == :red) ? "R" : "W"
   end
-
 end # END PIECE
+
+
+
+
+class NullPiece < Piece
+  def initialize
+    @color = nil
+  end
+
+  def to_s
+    " "
+  end
+end
+
+
+
+
+class King < Piece
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
